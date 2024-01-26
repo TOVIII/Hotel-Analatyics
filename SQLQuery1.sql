b98fdee0b4d926c466df4192a7ce84783fd43c12
Victor Mpofu <vicmpofu20@gmail.com>
2024-01-26 11:54:13 +0200

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,4 +6,9 @@ select* from dbo.[2019]
 union
 select* from dbo.[2020])
 
-select stays_in_week_nights+stays_in_weekend_nights from hotels
+select* from hotels
+left join dbo.market_segment
+on hotels.market_segment =market_segment.market_segment
+left join dbo.meal_cost
+on meal_cost.meal = hotels.meal
+
